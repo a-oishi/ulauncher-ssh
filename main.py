da42813c40fd07edf4626db27d51a2e713ec8e46
akihisa oishi <akihisa.oishi@gmail.com>
2019-01-27 08:01:52 +0900

    debug

--- main.py
+++ main.py
@@ -62,11 +62,11 @@ class SshExtension(Extension):
         home = expanduser("~")
 
         cmd = self.terminal_cmd.replace("%SHELL", shell).replace("%CONN", addr)
-        logger.info(cmd)
-        logger.info(shell)
-        logger.info(home)
-        logger.info(self.terminal)
-        logger.info(self.terminal_arg)
+        logger.error(cmd)
+        logger.error(shell)
+        logger.error(home)
+        logger.error(self.terminal)
+        logger.error(self.terminal_arg)
 
         if self.terminal:
             subprocess.Popen([self.terminal, self.terminal_arg, cmd], cwd=home)
